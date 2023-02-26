c281a3f0bcbd8a3c08c31e8e9b4a80a686f6aa24
Nikita Pisarev <nikgooder@gmail.com>
2023-02-26 21:32:50 +0300

    Removed an extra variable

--- HWs/HW1/InsertionSort/InsertionSort/Program.cs
+++ HWs/HW1/InsertionSort/InsertionSort/Program.cs
@@ -7,8 +7,7 @@ class Program
     static void PrintArray(int[] array)
     {
         Console.Write("[ ");
-        int sizeArray = array.Length;
-        for (int i = 0; i < sizeArray; ++i)
+        for (int i = 0; i < array.Length; ++i)
         {
             Console.Write($"{array[i]} ");
         }
@@ -24,8 +23,7 @@ class Program
 
     static void SortByInsertionSort(int[] array)
     {
-        int sizeArray = array.Length;
-        for (int i = 1; i < sizeArray; ++i)
+        for (int i = 1; i < array.Length; ++i)
         {
             int j = i;
             while (j != 0 && array[j] < array[j - 1])
