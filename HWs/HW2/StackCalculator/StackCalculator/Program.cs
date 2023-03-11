7f71190756d856d4c02fe19f9a70ab618bb3a2fc
Nikita Pisarev <nikgooder@gmail.com>
2023-03-11 19:23:34 +0300

    Add postfix notation calculator

--- HWs/HW2/StackCalculator/StackCalculator/Program.cs
+++ HWs/HW2/StackCalculator/StackCalculator/Program.cs
@@ -1,11 +1,25 @@
-﻿namespace Program;
+﻿namespace StackCalculator;
 
-using StackCalculator;
-
+using System;
 class Program
 {
     static void Main()
     {
+        Console.WriteLine("Hi, I'm a stack calculator, I can calculate an expression written in reverse Polish notation!");
+        Console.Write("Enter your expression: ");
+        string? expression = Console.ReadLine();
 
+        while (expression == null || expression == "")
+        {
+            Console.Write("Error! The expression can't be empty.\nTry again: ");
+            expression = Console.ReadLine();
+        }
+
+        var stackArray = new StackArray();
+        var stackCalculator = new StackCalculator(stackArray);
+
+        var result = stackCalculator.CalculateExpression(expression);
+
+        Console.WriteLine($"Answer: {result}\nGood luck!");
     }
 }
---
+++ HWs/HW2/StackCalculator/StackCalculator/StackCalculator.cs
@@ -0,0 +1,99 @@
+namespace StackCalculator;
+
+internal class StackCalculator
+{
+    public StackCalculator(IStack stack)
+    {
+        this.stack = stack;
+    }
+
+    private readonly IStack stack;
+
+    private static bool IsZero(double number)
+    {
+        return Math.Abs(number) < 0.0001;
+    }
+
+    private void Operations(string sign)
+    {
+        double firstElement = 0;
+        double secondElement = 0;
+
+        try
+        {
+            firstElement = stack.Pop();
+            secondElement = stack.Pop();
+        }
+        catch (InvalidOperationException)
+        {
+            throw new ArgumentException("Incorrect expression.");
+        }
+
+        switch (sign)
+        {
+            case "+":
+                stack.Push(firstElement + secondElement);
+                break;
+
+            case "-":
+                stack.Push(firstElement - secondElement);
+                break;
+
+            case "*":
+                stack.Push(firstElement * secondElement);
+                break;
+
+            case "/":
+                if (IsZero(secondElement))
+                {
+                    throw new DivideByZeroException();
+                }
+
+                stack.Push(firstElement / secondElement);
+                break;
+        }
+    }
+
+    public double CalculateExpression(string expression)
+    {
+        if (expression == null)
+        {
+            throw new ArgumentNullException(nameof(expression), "can't be null");
+        }
+
+        if (expression == string.Empty)
+        {
+            throw new ArgumentException(nameof(expression), "can't be empty");
+        }
+
+        string operations = "+-*/";
+        foreach (var element in expression.Split(" "))
+        {
+            if (operations.Contains(element))
+            {
+                Operations(element);
+            }
+            else
+            {
+                stack.Push(double.Parse(element));
+            }
+        }
+
+        double result = 0;
+        try
+        {
+            result = stack.Pop();
+        }
+        catch (InvalidOperationException)
+        {
+            throw new ArgumentException("Incorrect expression.");
+        }
+
+        if (!stack.IsEmpty())
+        {
+            throw new ArgumentException("Incorrect expression.");
+        }
+
+        return result;
+    }
+}
