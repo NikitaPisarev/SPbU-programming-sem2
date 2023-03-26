e0095fa4d152dbdad5bd08d04d1e4cdcd2d803cf
Nikita Pisarev <nikgooder@gmail.com>
2023-03-26 16:01:19 +0300

    Add test for GetAllCodes method

--- HWs/HW3/LZW/LZW/Utils/DecompressUtils.cs
+++ HWs/HW3/LZW/LZW/Utils/DecompressUtils.cs
@@ -3,14 +3,14 @@ namespace LZW;
 /// <summary>
 /// Utility class for LZW decompression.
 /// </summary>
-internal static class DecompressUtils
+public static class DecompressUtils
 {
     /// <summary>
     /// Getting a set of codes.
     /// </summary>
     /// <param name="bytes"> Compressed bytes. </param>
     /// <returns> A set of codes from compressed bytes. </returns>
-    internal static List<int> GetAllCodes(byte[] bytes)
+    public static List<int> GetAllCodes(byte[] bytes)
     {
         var buffer = new SequenceCodesBuffer();
 
@@ -54,7 +54,7 @@ internal static class DecompressUtils
     /// Creating and initializing a dictionary with the first 256 bytes.
     /// </summary>
     /// <returns> Dictionary of the first 256 codes. </returns>
-    internal static Dictionary<int, List<byte>> CreateAndInitializationOfDictionary()
+    public static Dictionary<int, List<byte>> CreateAndInitializationOfDictionary()
     {
         var dictionary = new Dictionary<int, List<byte>>();
         for (int i = 0; i < 256; ++i)
