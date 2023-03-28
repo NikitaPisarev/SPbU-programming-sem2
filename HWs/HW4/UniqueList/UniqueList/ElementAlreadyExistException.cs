6bf581a4cb6ad82a504092c7a060b8b4d9153980
Nikita Pisarev <nikgooder@gmail.com>
2023-03-28 15:59:39 +0300

    Add ElementAlreadyExistException

---
+++ HWs/HW4/UniqueList/UniqueList/ElementAlreadyExistException.cs
@@ -0,0 +1,18 @@
+namespace Lists;
+
+[Serializable]
+public class ElementAlreadyExistException : InvalidOperationException
+{
+    public ElementAlreadyExistException() { }
+
+    public ElementAlreadyExistException(string message) : base(message) { }
+
+    public ElementAlreadyExistException(string message, Exception inner)
+
+    : base(message, inner) { }
+
+    protected ElementAlreadyExistException(
+    System.Runtime.Serialization.SerializationInfo info,
+    System.Runtime.Serialization.StreamingContext context)
+    : base(info, context) { }
+}
