3379f8204f57f9df1aa26372c2bd78aea1550702
Nikita Pisarev <nikgooder@gmail.com>
2023-03-28 21:53:01 +0300

    Initial commit

---
+++ HWs/HW4/ParseTree/ParseTree/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+if (args.Length != 1)
+{
+    throw new IOException("Argument error. Use \"dotnet run help\".");
+}
+
+if (args[0] == "help")
+{
+    WriteLine("""
+        This program computes the expression by parsing tree.
+
+        Usage:
+        -------------------------------------------
+        dotnet run "<expression>"
+        -------------------------------------------
+
+        For example:
+
+        dotnet run "(* (+ 1 1) 2)"
+        -------------------------------------------
+        Output:
+        ( * ( + 1 1 ) 2 ) = 4
+
+        """);
+}
+else
+{
+    Write(args[0]);
+}
