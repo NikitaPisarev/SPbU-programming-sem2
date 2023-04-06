e58cdf1ee46b3f7e78f85622d0a242d9f3ea1b6e
Nikita Pisarev <nikgooder@gmail.com>
2023-04-06 23:56:08 +0300

    Add documentation

--- HWs/HW6/MapFilterFold/MapFilterFold/MapFilterFold.cs
+++ HWs/HW6/MapFilterFold/MapFilterFold/MapFilterFold.cs
@@ -1,7 +1,17 @@
 namespace MapFilterFold;
 
+/// <summary>
+/// A static class that provides Map, Filter, and Fold functions for lists.
+/// </summary>
 public static class MapFilterFold
 {
+    /// <summary>
+    /// Apply the function to each element of this list.
+    /// </summary>
+    /// <param name="list"> List of elements. </param>
+    /// <param name="function"> Function to apply. </param>
+    /// <typeparam name="TInp"> The type of elements in the list. </typeparam>
+    /// <typeparam name="TOut"> The type of elements in the resulting list. </typeparam>
     public static List<TOut> Map<TInp, TOut>(List<TInp> list, Func<TInp, TOut> function)
     {
         var result = new List<TOut>();
@@ -13,12 +23,18 @@ public static class MapFilterFold
         return result;
     }
 
-    public static List<T> Filter<T>(List<T> list, Func<T, bool> function)
+    /// <summary>
+    /// Getting elements from the list that satisfy the condition.
+    /// </summary>
+    /// <param name="list"> List of elements. </param>
+    /// <param name="predicate"> Predicate for checking items from this list. </param>
+    /// <typeparam name="T"> The type of elements in the list. </typeparam>
+    public static List<T> Filter<T>(List<T> list, Func<T, bool> predicate)
     {
         var result = new List<T>();
         foreach (var i in list)
         {
-            if (function(i))
+            if (predicate(i))
             {
                 result.Add(i);
             }
@@ -27,13 +43,21 @@ public static class MapFilterFold
         return result;
     }
 
-    public static TOut Fold<TInp, TOut>(List<TInp> list, TOut accumulate, Func<TOut, TInp, TOut> function)
+    /// <summary>
+    /// Accumulation of the original value using the accumulation function and the list that we are going through.
+    /// </summary>
+    /// <param name="list"> List of elements. </param>
+    /// <param name="accumulator"> Initial value of accumulator. </param>
+    /// <param name="function"> A function to apply to the accumulator and the next element from the list. </param>
+    /// <typeparam name="TInp"> The type of elements in the list. </typeparam>
+    /// <typeparam name="TOut"> Type of output element. </typeparam>
+    public static TOut Fold<TInp, TOut>(List<TInp> list, TOut accumulator, Func<TOut, TInp, TOut> function)
     {
         foreach (var i in list)
         {
-            accumulate = function(accumulate, i);
+            accumulator = function(accumulator, i);
         }
 
-        return accumulate;
+        return accumulator;
     }
 }
