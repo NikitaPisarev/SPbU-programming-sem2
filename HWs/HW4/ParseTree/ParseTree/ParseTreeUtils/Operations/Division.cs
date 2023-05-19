c0929a2be7d7f89a14c839b2012a8ef35abed332
Nikita Pisarev <nikgooder@gmail.com>
2023-05-19 17:29:07 +0300

    fix: made the delta a constant

--- HWs/HW4/ParseTree/ParseTree/ParseTreeUtils/Operations/Division.cs
+++ HWs/HW4/ParseTree/ParseTree/ParseTreeUtils/Operations/Division.cs
@@ -5,13 +5,15 @@ namespace ParseTree;
 /// </summary>
 internal class Division : Operation
 {
+    private const double _delta = 0.001;
+
     /// <summary>
     /// Computing the division operation.
     /// </summary>
     /// <exception cref="DivideByZeroException" > Divisor cannot be zero. </exception>
     public override double Calculate()
     {
-        if (Math.Abs(RightChild!.Calculate()) < 0.001)
+        if (Math.Abs(RightChild!.Calculate()) < _delta)
         {
             throw new DivideByZeroException();
         }
