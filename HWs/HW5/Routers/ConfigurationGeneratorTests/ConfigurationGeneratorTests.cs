b4517290cb524e506363bfad294a0f991ed19671
Nikita Pisarev <nikgooder@gmail.com>
2023-05-19 18:33:40 +0300

    fix: add a tests for incorrect input

--- HWs/HW5/Routers/ConfigurationGeneratorTests/ConfigurationGeneratorTests.cs
+++ HWs/HW5/Routers/ConfigurationGeneratorTests/ConfigurationGeneratorTests.cs
@@ -10,7 +10,8 @@ public class Tests
         var network = """
         1: 2 (10), 3 (5)
         2: 3 (1)
-        """;
+        """.Replace("\r", "");
+
         var expected = """
         1: 2 (10), 3 (5)
         """.Replace("\r", "");
@@ -32,7 +33,8 @@ public class Tests
         6: 9 (11)
         7: 8 (30)
         8: 9 (15)
-        """;
+        """.Replace("\r", "");
+
         var expected = """
         1: 5 (11), 2 (6)
         2: 7 (12)
@@ -53,6 +55,50 @@ public class Tests
     {
         var network = """
         1: 3 (4), 2 (1), 6 (5), 5 (6)
+        4: 7 (3)
+        """;
+
+        Assert.Throws<ArgumentException>(() => ConfigurationGenerator.CreateOptimalConfiguration(network));
+    }
+
+    [Test]
+    public void CreateOptimalConfiguration_IncorrectNetwork1_ArgumentException()
+    {
+        var network = """
+        1 3 (4), 2 (1), 6 (5), 5 (6)
+        4: 7 (3)
+        """;
+
+        Assert.Throws<ArgumentException>(() => ConfigurationGenerator.CreateOptimalConfiguration(network));
+    }
+
+    [Test]
+    public void CreateOptimalConfiguration_IncorrectNetwork2_ArgumentException()
+    {
+        var network = """
+        1: 3 (((4))), 2 (1), 6 (5), 5 (6)
+        4: 7 (3)
+        """;
+
+        Assert.Throws<ArgumentException>(() => ConfigurationGenerator.CreateOptimalConfiguration(network));
+    }
+
+    [Test]
+    public void CreateOptimalConfiguration_IncorrectNetwork3_ArgumentException()
+    {
+        var network = """
+        1: 3 (4), 2 (1), 6 (5), 5 (6),
+        4: 7 (3)
+        """;
+
+        Assert.Throws<ArgumentException>(() => ConfigurationGenerator.CreateOptimalConfiguration(network));
+    }
+
+    [Test]
+    public void CreateOptimalConfiguration_IncorrectNetwork4_ArgumentException()
+    {
+        var network = """
+        1: 3 (4), 2 (A1), 6 (5), 5 (6)
         4: 7 (3)
         """;
 
