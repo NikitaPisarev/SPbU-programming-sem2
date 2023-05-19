b58818e96f922e3974b2f15b7bb92047c993affa
Nikita Pisarev <nikgooder@gmail.com>
2023-05-19 15:59:10 +0300

    feat: done priority queue with linear asymptotics

--- Tests/finalTest/Task2/QueueTests/QueueTests.cs
+++ Tests/finalTest/Task2/QueueTests/QueueTests.cs
@@ -5,7 +5,7 @@ using PriorityQueue;
 public class QueueTests
 {
     [Test]
-    public void EnqueueAndDequeue_CorrectInput_CorrectResult()
+    public void EnqueueAndDequeue1_CorrectInput_CorrectResult()
     {
         var queue = new PriorityQueue<int>();
         var expected = new int[5] { 1, 2, 3, 4, 5 };
@@ -14,6 +14,24 @@ public class QueueTests
         queue.Enqueue(4, 1);
         queue.Enqueue(5, 1);
         queue.Enqueue(3, 5);
+        queue.Enqueue(2, 10);
+
+        var actual = new List<int>();
+        while (!queue.Empty)
+        {
+            actual.Add(queue.Dequeue());
+        }
+
+        Assert.That(expected, Is.EqualTo(actual.ToArray()));
+    }
+
+    [Test]
+    public void EnqueueAndDequeue2_CorrectInput_CorrectResult()
+    {
+        var queue = new PriorityQueue<int>();
+        var expected = new int[2] { 2, 1 };
+
+        queue.Enqueue(1, 1);
         queue.Enqueue(2, 10);
 
         var actual = new List<int>();
